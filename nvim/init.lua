6208a91a7f2db272104198058c6795ac09a60cda
Mike Felida <mike@m3f5.dev>
2025-09-04 12:37:13 +0200

    true color plz

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,6 +98,8 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.o.termguicolors = true
+
 -- Make line numbers default
 vim.o.number = true
 vim.o.relativenumber = true
