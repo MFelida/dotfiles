d7813c37d70fecff6a6b058a3b5d9c5e32da064d
Mike Felida <mike@m3f5.dev>
2025-11-05 22:25:54 +0100

    idk

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 
-to_root=( bash_profile bashrc bashrc.d tmux.conf )
-for f in "${to_root[@]}"; do
-	ln -srf ${f} "${HOME}/.${f}"
+CWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+for f in {bash_profile,bashrc,bashrc.d,tmux.conf}; do
+	ln -sf $CWD/${f} "${HOME}/.${f}"
 done
-unset to_root
 
-ln -sfrt ${XDG_CONFIG_HOME:-"$HOME/.config"} alacritty fish ghostty nvim tmux-config
+for d in {alacritty,fish,ghostty,nvim,tmux-config}; do
+	ln -sf ${CWD}/${d} ${XDG_CONFIG_HOME:-"$HOME/.config"}
+done
